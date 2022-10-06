f183d7e46cfc1e6bf216c23b54960a42351681ea
olmartin <olmartin@student.42lausanne.ch>
2022-10-06 16:09:13 +0200

    ex04 en cours

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,14 +3,32 @@
 #include <iostream>
 #include <fstream>
 
+std::string	replace(std::string str)
+{
+	return (str);
+}
+
+
+
 int	main(int argc, char **argv)
 {
-	std::ifstream	ifs(argv[1]);
+	std::string filename;
+	std::string res;
+	if (argc != 4)
+	{
+		std::cout << "Error with arguments !" << std::endl;
+		return (1);
+	}
+	std::ifstream	inFile(argv[1]);
+	filename = argv[1];
+	filename.append(".replace");
+	std::ofstream	outFile("replaceFile");
 
 
+	inFile >> res;
+		std::cout << res << std::endl;
 
-
-	ifs.close();
-
+	inFile.close();
+	outFile.close();
 	return 0;
 }
